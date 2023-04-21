a3b34c1a23b9f8f5a89a044a242067aa5ff656ba
iscodevdt <blesskaffeegt@gmail.com>
2023-04-21 03:02:38 -0600

    gift

---
+++ src/GiftExpert.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const GiftExpert = () => {
+
+    const [categories , setCategories ] = useState(['Fire'])
+
+    console.log(categories)
+
+
+  return (
+    <>
+     <h1>GiftExpertApp</h1> 
+    </>
+  )
+}
+
+export default GiftExpert
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GiftExpert from './GiftExpert';
+
+// estilos de css
+import './styles.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <GiftExpert />
+  </React.StrictMode>,
+)
