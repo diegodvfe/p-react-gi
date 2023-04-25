bdf05c4ae359e10c6f16e191bd825f295a38c433
iscodevdt <blesskaffeegt@gmail.com>
2023-04-25 08:14:44 -0600

    refactor-app

--- src/GiftExpert.jsx
+++ src/GiftExpert.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
-import { AddCategory } from "./components/AddCategory"
-import { GiftGrid } from "./components/GiftGrid"
+import { AddCategory, GiftGrid } from "./components/index"
+
 
 
 const GiftExpert = () => {
--- src/components/GiftGrid.jsx
+++ src/components/GiftGrid.jsx
@@ -1,38 +1,33 @@
-
-import { useEffect, useState } from 'react';
 // import { getGifs } from './../helpers/getGifs';
 
-
-import { GiftItem } from './GiftItem';
-import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GiftItem } from "./GiftItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GiftGrid = ({ category }) => {
+   const { images, isLoading } = useFetchGifs(category);
 
-    const {images, isLoading} = useFetchGifs (category)
+   return (
+      <>
+         <h3>{category}</h3>
 
+         {
+            isLoading && (<h2>Cargando...</h2> )
+         }
 
-  return (
-    <>
-        <h3>{category}</h3>
+         <div className="card-grid">
+            {images.map((image) => (
+               <GiftItem
+                  key={image.id}
+                  {...image}
+               />
+            ))}
+         </div>
 
-        
-    <div className='card-grid'>
-
-        {
-        images.map((image) => (
-            <GiftItem key={image.id}
-            {...image}
-            />
-        ))
-        }
-       
-    </div>
-
-        {/* {
+         {/* {
             listGift.map( listGift => (
                 <p key={category}> {listGift} </p>
             ))
         } */}
-    </>
-  )
-}
+      </>
+   );
+};
